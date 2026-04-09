5d9a4962362748ec162fe422cceba9b961994c24
Atanas Dimitrov <hi.atanasdim@gmail.com>
2026-04-09 20:36:04 +0100

    Add x and y rotation on pointer move

--- components/pill-button/PillButton.tsx
+++ components/pill-button/PillButton.tsx
@@ -1,40 +1,79 @@
+"use client";
+
 import "./styles.css";
 
+import {
+  ButtonHTMLAttributes,
+  PointerEventHandler,
+  PropsWithChildren,
+  useRef,
+} from "react";
 import { twMerge } from "tailwind-merge";
 
-type PillButtonProps = {
-  children?: string;
-  className?: string;
-};
+type PillButtonProps = PropsWithChildren<
+  ButtonHTMLAttributes<HTMLButtonElement>
+>;
 
-const PillButton = ({ children = "Launch", className }: PillButtonProps) => {
+const PillButton = ({
+  children = "Launch",
+  className,
+  onPointerMove,
+  onPointerUp,
+  ...rest
+}: PillButtonProps) => {
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  const handlePointerMove: PointerEventHandler<HTMLButtonElement> = (e) => {
+    if (!btnRef.current) return;
+    // here we have to check the x position of the pointer relative to the button's center, and set a CSS variable that will be used in the hover transform
+    const rect = btnRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+    btnRef.current.style.setProperty("--btn-rotate-y", `${x * 0.05}deg`);
+    btnRef.current.style.setProperty("--btn-rotate-x", `${y * -0.5}deg`);
+    onPointerMove?.(e);
+  };
+
+  const handlePointerUp: PointerEventHandler<HTMLButtonElement> = (e) => {
+    if (!btnRef.current) return;
+    // reset the CSS variable on pointer up, so the button goes back to its original position
+    btnRef.current.style.setProperty("--btn-rotate-x", `0deg`);
+    btnRef.current.style.setProperty("--btn-rotate-y", `0deg`);
+
+    onPointerUp?.(e);
+  };
+
   return (
-    <div className="relative z-10 perspective-dramatic">
-      <div className="absolute inset-0 -z-1 -translate-x-1/2 translate-y-1/3 bg-blue-400" />
+    <div className="relative z-10 perspective-[200px]">
+      <div className="absolute inset-0 -z-1 size-5/4 -translate-x-1/2 translate-y-1/3 rounded-xl bg-emerald-800" />
       <button
+        ref={btnRef}
         type="button"
+        {...rest}
         className={twMerge(
           // base styles
-          "group noise relative inline-flex min-w-40 items-center justify-center rounded-full px-6 py-3",
+          "jellybean-btn group noise relative inline-flex min-w-40 items-center justify-center rounded-full px-6 py-3.5",
           // typography
-          "text-sm font-medium tracking-tight text-white",
+          "text-sm font-medium tracking-tight text-white text-shadow-[0_0px_6px_rgba(10,10,10,0.4)]",
           // base background
-          "bg-pink-500/60 backdrop-blur-sm",
+          "bg-(--btn-bg-color) backdrop-blur-sm",
           // press effect layers
-          "before:absolute before:top-1/2 before:left-1/2 before:h-1/2 before:w-7/10 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-pink-50/60 before:opacity-0 before:blur-sm before:transition-opacity before:duration-300 active:before:opacity-100",
-          "after:absolute after:top-2/5 after:left-1/2 after:h-1/3 after:w-7/10 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-pink-700/50 after:opacity-0 after:blur-sm after:transition-opacity after:duration-300 active:after:opacity-100",
+          "before:absolute before:top-1/2 before:left-1/2 before:h-1/2 before:w-7/10 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-(--btn-press-highlight) before:opacity-0 before:blur-sm before:transition-opacity before:duration-300 active:before:opacity-100",
+          "after:absolute after:top-2/5 after:left-1/2 after:h-1/3 after:w-7/10 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-(--btn-press-shadow) after:opacity-0 after:blur-sm after:transition-opacity after:duration-300 active:after:opacity-100",
           // focus ring
           "focus-visible:ring-2 focus-visible:ring-neutral-400 focus-visible:ring-offset-2 focus-visible:outline-none",
           // shadows
           "shadow-[inset_0px_-4px_16px_0px_rgba(10,10,10,0.4),0_12px_16px_-14px_rgba(10,10,10,0.55)]",
           "hover:shadow-[inset_0px_-4px_16px_0px_rgba(10,10,10,0.4),0_16px_36px_-14px_rgba(10,10,10,0.55)]",
           // transforms
-          "hover:-translate-y-0.5 hover:scale-[1.02] hover:rotate-y-(--btn-rotate-y)",
+          "hover:-translate-y-0.5 hover:scale-[1.02] hover:rotate-x-(--btn-rotate-x) hover:rotate-y-(--btn-rotate-y)",
           "active:translate-y-0 active:scale-[0.99]",
 
           "transition-all duration-300 ease-out",
           className,
         )}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
       >
         <span className="z-1 transition-transform duration-300 group-hover:-translate-y-px group-active:translate-y-0 group-active:scale-98">
           {children}
